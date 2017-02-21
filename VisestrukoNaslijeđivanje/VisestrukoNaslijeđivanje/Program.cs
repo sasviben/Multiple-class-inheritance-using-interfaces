724eefd971ad0057018d5e4db3c6b892ba82d214
sandro sviben <sandro.sviben@hotmail.com>
2017-02-21 12:22:11 +0100

    Primjer visetrukog naslijedjivanja uz pomoc interface-a

---
+++ VisestrukoNaslijeđivanje/VisestrukoNaslijeđivanje/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VisestrukoNaslijeđivanje
+{
+    interface IA
+    {
+        void PrintA();
+    }
+    class A : IA
+    {
+        public void PrintA()
+        {
+            Console.WriteLine("A");
+        }
+    }
+    interface IB
+    {
+        void PrintB();
+    }
+    class B : IB
+    {
+        public void PrintB()
+        {
+            Console.WriteLine("B");
+        }
+    }
+    class AB : IA,IB
+    {
+        A a = new A();
+        B b = new B();
+
+        public void PrintA()
+        {
+            a.PrintA();
+        }
+        public void PrintB()
+        {
+            b.PrintB();
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AB ab = new AB();
+            ab.PrintA();
+            ab.PrintB();
+        }
+    }
+}
